3c5b271c6537c3b726ef1aa808a169e2a06acb98
William DURAND <william.durand1@gmail.com>
2012-05-25 10:56:35 +0200

    Added show action

--- src/Mazenovi/TodoMVCBundle/Controller/DefaultController.php
+++ src/Mazenovi/TodoMVCBundle/Controller/DefaultController.php
@@ -7,7 +7,6 @@ use Symfony\Component\HttpFoundation\Request,
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route,
     Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-//use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 use FOS\RestBundle\Controller\Annotations\Prefix,
     FOS\RestBundle\Controller\Annotations\NamePrefix,
@@ -18,54 +17,75 @@ use FOS\RestBundle\Controller\Annotations\Prefix,
 use Mazenovi\TodoMVCBundle\Model\TodoQuery,
     Mazenovi\TodoMVCBundle\Model\Todo;
 
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
 /**
-* @Route("todos")
-* @NamePrefix("todos_rest_")
-*/
+ * @Route("todos")
+ * @NamePrefix("todos_rest_")
+ */
 
 class DefaultController extends Controller
 {
     /**
-    * For Rest Routing
-    * see http://www.slideshare.net/Wombert/designing-http-interfaces-and-restful-web-services-phpday2012-20120520#65
-    * see http://www.slideshare.net/Wombert/designing-http-interfaces-and-restful-web-services-phpday2012-20120520#66
-    */
+     * For Rest Routing
+     * see http://www.slideshare.net/Wombert/designing-http-interfaces-and-restful-web-services-phpday2012-20120520#65
+     * see http://www.slideshare.net/Wombert/designing-http-interfaces-and-restful-web-services-phpday2012-20120520#66
+     */
 
     /**
-    * List all todos.
-    *
-    * @Route("/", defaults = { "_format" = "~" })
-    * @Method({"GET"})
-    * @View()
-    */
+     * List all todos.
+     *
+     * @Route("/", defaults = { "_format" = "~" })
+     * @Method({"GET"})
+     * @View()
+     */
     public function indexAction()
     {
         return TodoQuery::create()->find();
     }
 
     /**
-    * Create a new todo.
-    *
-    * @Route("/", defaults = { "_format" = "~" })
-    * @Method({"POST"})
-    * @View(statusCode=201)
-    */
-    public function createAction(Request $request)
+     * @Route("/{id}", defaults = { "_format" = "~" })
+     * @Method({"GET"})
+     * @View()
+     */
+    public function showAction(Todo $todo)
     {
-        $values = $request->request->all();
-        $todo = new Todo();
-        $todo->setContent($values['content']);
-        $todo->save();
+        return $todo;
     }
 
     /**
-    * update an existing todo.
-    *
-    * @Route("/{id}", defaults = { "_format" = "~" }, requirements = { "id" = "\d+" })
-    * @Method({"PUT"})
-    * @View(statusCode=200)
-    * @View()
-    */
+     * Create a new todo.
+     *
+     * @Route("/", defaults = { "_format" = "~" })
+     * @Method({"POST"})
+     * @View(statusCode=201)
+     */
+    public function createAction(Request $request)
+    {
+        $values = $request->request->all();
+
+        $todo = new Todo();
+        $todo->setContent($values['content']);
+        $todo->save();
+
+        $url = $this->get('router')->generate(
+            'mazenovi_todomvc_default_show',
+            array('id' => $todo->getId()),
+            true
+        );
+
+        return array('url' => $url);
+    }
+
+    /**
+     * update an existing todo.
+     *
+     * @Route("/{id}", defaults = { "_format" = "~" }, requirements = { "id" = "\d+" })
+     * @Method({"PUT"})
+     * @View(statusCode=200)
+     * @View()
+     */
     public function updateAction(Request $request)
     {
         $values = $request->request->all();
@@ -77,15 +97,15 @@ class DefaultController extends Controller
     }
 
     /**
-    * delete an existing todo.
-    *
-    * @Route("/{id}", defaults = { "_format" = "~" }, requirements = { "id" = "\d+" })
-    * @Method({"DELETE"})
-    * @View(statusCode=200)
-    */
+     * delete an existing todo.
+     *
+     * @Route("/{id}", defaults = { "_format" = "~" }, requirements = { "id" = "\d+" })
+     * @Method({"DELETE"})
+     * @View(statusCode=200)
+     */
     public function deleteAction(Request $request)
     {
         $todo = TodoQuery::create()->findOneById($request->get('id'));
         $todo->delete();
     }
-}
+    }
